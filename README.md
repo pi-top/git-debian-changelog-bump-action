01b8cea6381aafd3d25534cde7c523d96d964693
Mike Roberts <2947595+m-roberts@users.noreply.github.com>
2021-07-28 17:15:17 +0100

    Add GitHub Action support (#1)

--- README.md
+++ README.md
@@ -1,5 +1,41 @@
-# pi-top Docker image for [generating Debian changelog from git commit messages](https://manpages.debian.org/buster/git-buildpackage/gbp-dch.1.en.html)
+# GitHub Action: a git-based Debian package changelog bumper
 
-## Featured Repositories
+pi-top's Docker-based GitHub Action for [generating an updated Debian changelog from git commit messages](https://manpages.debian.org/buster/git-buildpackage/gbp-dch.1.en.html)
+This Action is used across all of pi-topOS's Debian packaging.
 
-[pi-top Python SDK](https://github.com/pi-top/pi-top-Python-SDK)
+Snapshot numbers are based off of the number of commits from:
+  * the latest semver tag
+  * the first commit in its history
+
+### Notes
+You need to check out your code with `fetch-depth: 0` so that tags are fetched.
+
+### Examples
+
+Increment package version to snapshot, and store new version in env:
+```
+env:
+  CHANGELOG_AUTHOR_NAME: "pi-top"
+  CHANGELOG_AUTHOR_EMAIL: "deb-maintainers@pi-top.com"
+
+jobs:
+  build-debian-package:
+    runs-on: ubuntu-20.04
+    steps:
+      - name: Checkout code
+        uses: actions/checkout@v2.2.0
+        with:
+          fetch-depth: 0
+
+      - name: Patch changelog (snapshot)
+        uses: pi-top/git-debian-changelog-bump-action@master
+        with:
+          release: false
+          author_name: ${{ env.CHANGELOG_AUTHOR_NAME }}
+          author_email: ${{ env.CHANGELOG_AUTHOR_EMAIL }}
+
+      - name: Determine current version
+        run: |
+          sudo apt install -y dpkg-dev
+          echo "CURRENT_VERSION=$(dpkg-parsechangelog -Sversion)" >> $GITHUB_ENV
+```
---
+++ main.js
@@ -0,0 +1,202 @@
+const core = require("@actions/core")
+const exec = require("@actions/exec")
+const firstline = require("firstline")
+const hub = require("docker-hub-utils")
+const path = require("path")
+const fs = require("fs")
+
+async function main() {
+    try {
+        const authorName = core.getInput("author_name") || ""
+        const authorEmail = core.getInput("author_email") || ""
+
+        if (authorName === "" || authorEmail === "") {
+            throw "Invalid author information"
+        }
+
+        const isReleaseVersion = core.getInput("release").toLowerCase() === 'true' || false
+        const sourceRelativeDirectory = core.getInput("source_directory") || "./"
+
+        const workspaceDirectory = process.cwd()
+        const sourceDirectory = path.join(workspaceDirectory, sourceRelativeDirectory)
+
+        const file = path.join(sourceDirectory, "debian/changelog")
+        const changelog = await firstline(file)
+        const regex = /^(?<pkg>.+) \(((?<epoch>[0-9]+):)?(?<version>[^:-]+)(-(?<revision>[^:-]+))?\) (?<packageDistribution>.+);/
+        const match = changelog.match(regex)
+        const { pkg, epoch, version, revision, packageDistribution } = match.groups
+
+        const container = pkg
+
+        core.startGroup("Print details")
+        const details = {
+            pkg: pkg,
+            epoch: epoch,
+            version: version,
+            revision: revision,
+            workspaceDirectory: workspaceDirectory,
+            authorName: authorName,
+            authorEmail: authorEmail,
+            isReleaseVersion: isReleaseVersion,
+            sourceRelativeDirectory: sourceRelativeDirectory
+        }
+        console.log(details)
+        core.endGroup()
+
+        core.startGroup("Create container")
+        await exec.exec("docker", [
+            "create",
+            "--name", container,
+            "--volume", workspaceDirectory + ":" + workspaceDirectory,
+            "--workdir", sourceDirectory,
+            "--env", "DEBIAN_FRONTEND=noninteractive",
+            "--env", "DPKG_COLORS=always",
+            "--env", "FORCE_UNSAFE_CONFIGURE=1",
+            "--env", "DEBFULLNAME=" + authorName,
+            "--env", "DEBEMAIL=" + authorEmail,
+            "--tty",
+            "pitop/git-buildpackage:latest",
+            "sleep", "inf"
+        ])
+        core.endGroup()
+
+        core.startGroup("Back up changelog") 
+        await exec.exec("cp", [
+            sourceDirectory + "/debian/changelog",
+            "/tmp/changelog.orig",
+        ])
+        core.endGroup()
+
+        core.startGroup("Start container")
+        await exec.exec("docker", [
+            "start",
+            container
+        ])
+        core.saveState("container", container)
+        core.endGroup()
+
+        core.startGroup("Retrieve tags & base commits for changelog")
+        let sinceTagStdout = "";
+        const sinceTagOptions = {}
+        sinceTagOptions.listeners = {
+            stdout: (data) => {
+                sinceTagStdout += data.toString();
+            }
+        }
+        await exec.exec("docker", [
+            "exec",
+            container,
+            "git",
+            "tag",
+            "-l",
+            "v*"
+        ], sinceTagOptions)
+        sinceTag = sinceTagStdout.split("\n");
+        sinceTag = sinceTag.sort()[sinceTag.length - 1];
+
+        let sinceCommitStdout = "";
+        const sinceCommitOptions = {};
+        sinceCommitOptions.listeners = {
+            stdout: (data) => {
+                sinceCommitStdout += data.toString();
+            }
+        }
+        let snapshotNumber = "";
+        const snapshotNumberOptions = {}
+        snapshotNumberOptions.listeners = {
+            stdout: (data) => {
+                snapshotNumber += data.toString();
+            }
+        }
+        if (sinceTag === undefined) {
+            // Get last entry in git log (i.e. first commit in tree)
+            await exec.exec("docker", [
+                "exec",
+                container,
+                "git", "log", "--pretty=format:%H"
+            ],
+            sinceCommitOptions)
+
+            sinceCommit = sinceCommitStdout.split("\n")
+            sinceCommit = sinceCommit[sinceCommit.length - 1]
+
+            await exec.exec("docker", [
+                "exec",
+                container,
+                "git", "rev-list", "--count", "HEAD",
+                snapshotNumberOptions
+            ])
+
+            console.log("No version tags found - using number of commits to current branch for snapshot number")
+        } else {
+            console.log("Found version tag: " + sinceTag)
+
+            await exec.exec("docker", [
+                "exec",
+                container,
+                "git", "show-ref", "-s",
+                sinceTag
+            ], sinceCommitOptions)
+
+            sinceCommit = sinceCommitStdout.split("\n")[0]
+
+            await exec.exec("docker", [
+                "exec",
+                container,
+                "git", "rev-list", "--count",
+                sinceCommit + "...HEAD"
+            ], snapshotNumberOptions)
+        }
+
+        console.log("Since: " + sinceCommit)
+        console.log("Snapshot Number: " + snapshotNumber)
+        core.endGroup()
+
+        core.startGroup("Bump changelog")
+        await exec.exec("docker", [
+            "exec",
+            container,
+            "gbp", "dch", "--verbose", "--ignore-branch",
+            "--snapshot",
+            "--since=" + sinceCommit,
+            "--snapshot-number=" + snapshotNumber
+        ])
+        
+        if (isReleaseVersion) {
+            let distroStdout = "";
+            const distroOptions = {}
+            distroOptions.listeners = {
+                stdout: (data) => {
+                    distroStdout += data.toString();
+                }
+            }
+            await exec.exec("docker", [
+                "exec",
+                container,
+                "lsb_release", "-cs"
+            ], distroOptions)
+            distro = distroStdout.split("\n")[0];
+
+            await exec.exec("docker", [
+                "exec",
+                container,
+                "gbp", "dch", "--verbose", "--ignore-branch",
+                "--release",
+                "--distribution=" + distro,
+                "--spawn-editor=snapshot"
+            ])
+        }
+        core.endGroup()
+
+        core.startGroup("Show diff")
+        await exec.exec("diff", [
+            sourceDirectory + "/debian/changelog",
+            "/tmp/changelog.orig"
+        ], { ignoreReturnCode: true })
+        core.endGroup()
+    } catch (error) {
+        core.setFailed(error.message)
+    }
+}
+
+main()
